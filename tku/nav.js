43f8d562ae911e11f7f862a49bc188a7f6cd2127
john-1123 <56575351+john-1123@users.noreply.github.com>
2019-12-29 22:28:44 +0800

    Update nav.js

--- tku/nav.js
+++ tku/nav.js
@@ -7,24 +7,24 @@ function change(index) {
       break;
 
     case 2:
-      p.innerHTML = "<iframe width=100% height=100% src='./w01/pokemon.html' >";
+      p.innerHTML = "<iframe width=100% height=100% src='tku/w01/pokemon.html' >";
       break;
 
     case 3:
       p.innerHTML =
-        "<iframe width=100% height=100% src='./w02/imageGallery.html' >";
+        "<iframe width=100% height=100% src='tku/w02/imageGallery.html' >";
       break;
 
     case 4:
-      p.innerHTML = "<iframe width=100% height=100% src='./w02/blog.html' >";
+      p.innerHTML = "<iframe width=100% height=100% src='tku/w02/blog.html' >";
       break;
 
     case 5:
-      p.innerHTML = "<iframe width=100% height=100% src='./w03/div.html' >";
+      p.innerHTML = "<iframe width=100% height=100% src='tku/w03/div.html' >";
       break;
 
     case 6:
-      p.innerHTML = "<iframe width=100% height=100% src='./w04/tku60.html' >";
+      p.innerHTML = "<iframe width=100% height=100% src='tku/w04/tku60.html' >";
       break;
   }
 }
